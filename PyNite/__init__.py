6913bbbb3326faf43a164bdb03c8587bc178d19b
Craig <33323539+JWock82@users.noreply.github.com>
2018-09-08 09:20:14 -0600

    Delete __init__.py

--- PyNite/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov  7 19:08:48 2017
-
-@author: D. Craig Brinck, SE
-"""
-
-# Select libraries that will be imported into PyNite for the user
-from PyNite.FEModel3D import FEModel3D
