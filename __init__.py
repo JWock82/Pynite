51823431894aa4961e71515c6c42e29582885e99
Craig <33323539+JWock82@users.noreply.github.com>
2018-09-08 09:22:57 -0600

    Multiple New Features Added
    Added the ability to calculate max/min internal member forces and support reactions. Still have a few more features to add and then better documentation will come next.

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  7 19:08:48 2017
+
+@author: D. Craig Brinck, SE
+"""
+
+# Select libraries that will be imported into PyNite for the user
+from PyNite.FEModel3D import FEModel3D
