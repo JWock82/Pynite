277da1895e2f2825d30db9238392e80175609c01
FJFranklin <fjf@alinameridon.com>
2019-07-22 14:16:50 +0100

    Possible fix for rotations in end reactions.

--- PyNite/Member3D.py
+++ PyNite/Member3D.py
@@ -367,14 +367,14 @@ class Member3D():
     def F(self):
         
         # Calculate and return the global force vector
-        return matmul(self.T(), self.f())
+        return matmul(transpose(self.T()), self.f())
     
 #%% 
     # Global fixed end reaction vector
     def FER(self):
         
         # Calculate and return the fixed end reaction vector
-        return matmul(self.T(), self.fer())
+        return matmul(transpose(self.T()), self.fer())
 
 #%%
     def D(self):
