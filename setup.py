06e3d18936d0c8645d5099ca6407f72961823cbc
D. Craig Brinck <cbrinck@outlook.com>
2019-07-29 21:13:47 -0600

    Added 'setup.py'

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyNite",
+    version="0.0.1",
+    author="D. Craig Brinck, SE",
+    author_email="Building.Code@outlook.com",
+    description="A 3D frame, beam, and truss finite element package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JWock82/PyNite.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
