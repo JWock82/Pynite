492994f202361e04914cdf8791c7697cbb0fb5c1
D. Craig Brinck <cbrinck@outlook.com>
2019-07-29 21:26:39 -0600

    Changed 'PyNite' to 'PyNiteFEA' for PyPI

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyNite",
+    name="PyNiteFEA",
     version="0.0.1",
     author="D. Craig Brinck, SE",
     author_email="Building.Code@outlook.com",
