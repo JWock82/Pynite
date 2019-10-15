2e3e2ed4f80b3a34f620499c9e880479aaa9fc95
D. Craig Brinck <cbrinck@outlook.com>
2019-10-14 19:45:46 -0600

    Added end release test

---
+++ Testing/End Release Test.py
@@ -0,0 +1,31 @@
+# Simple beam created by using an end release at a fixed support
+# Units used in this example are inches, and kips
+
+# Import `FEModel3D` from `PyNite`
+from PyNite import FEModel3D
+from PyNite import Visualization
+
+myModel = FEModel3D()
+
+myModel.AddNode('N1', 0, 0, 0)
+#myModel.AddNode('N2', 0, 10*12, 0)
+myModel.AddNode('N2', 10*12, 0, 0)
+
+myModel.DefineSupport('N1', True, True, True, True, True, True)
+myModel.DefineSupport('N2', True, True, True, False, False, False)
+
+myModel.AddMember("M1", "N1", "N2", 29000, 11400, 100, 150, 250, 10)
+
+myModel.DefineReleases('M1', False, False, False, False, False, True, \
+                             False, False, False, False, False, False)
+
+myModel.AddMemberDistLoad('M1', 'Fy', 0.5, 0.5)
+
+Visualization.RenderModel(myModel)
+
+myModel.Analyze()
+
+myModel.GetMember('M1').PlotShear('Fy')
+myModel.GetMember('M1').PlotMoment('Mz')
+print('Calculated moment: ' + str(myModel.GetMember('M1').MaxMoment('Mz')))
+print('Expected moment: ' + str(0.5*(10*12)**2/8))
