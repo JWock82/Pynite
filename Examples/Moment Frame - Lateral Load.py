247b6f69aee29f34355f4642a6cf66fb4a00137a
D. Craig Brinck <cbrinck@outlook.com>
2020-03-21 09:44:33 -0600

    Updated example for load combinations

--- Examples/Moment Frame - Lateral Load.py
+++ Examples/Moment Frame - Lateral Load.py
@@ -9,43 +9,48 @@ from PyNite import Visualization
 MomentFrame = FEModel3D()
 
 # Add nodes (frame is 15 ft wide x 12 ft tall)
-MomentFrame.AddNode("N1", 0, 0, 0)
-MomentFrame.AddNode("N2", 0, 12*12, 0)
-MomentFrame.AddNode("N3", 15*12, 12*12, 0)
-MomentFrame.AddNode("N4", 15*12, 0*12, 0)
+MomentFrame.AddNode('N1', 0, 0, 0)
+MomentFrame.AddNode('N2', 0, 12*12, 0)
+MomentFrame.AddNode('N3', 15*12, 12*12, 0)
+MomentFrame.AddNode('N4', 15*12, 0*12, 0)
 
 # Add columns with the following properties:
 # E = 29000 ksi, G = 11400 ksi, Iy = 100 in^4, Iz = 150 in^4, J = 250 in^4, A = 10 in^2
-MomentFrame.AddMember("M1", "N1", "N2", 29000, 11400, 100, 150, 250, 10)
-MomentFrame.AddMember("M2", "N4", "N3", 29000, 11400, 100, 150, 250, 10)
+MomentFrame.AddMember('M1', 'N1', 'N2', 29000, 11400, 100, 150, 250, 10)
+MomentFrame.AddMember('M2', 'N4', 'N3', 29000, 11400, 100, 150, 250, 10)
 
 # Add a beam with the following properties:
 # E = 29000 ksi, G = 11400 ksi, Iy = 100 in^4, Iz = 250 in^4, J = 250 in^4, A = 15 in^2
-MomentFrame.AddMember("M3", "N2", "N3", 29000, 11400, 100, 250, 250, 15)
+MomentFrame.AddMember('M3', 'N2', 'N3', 29000, 11400, 100, 250, 250, 15)
 
 # Provide fixed supports at the bases of the columns
-MomentFrame.DefineSupport("N1", True, True, True, True, True, True)
-MomentFrame.DefineSupport("N4", True, True, True, True, True, True)
+MomentFrame.DefineSupport('N1', True, True, True, True, True, True)
+MomentFrame.DefineSupport('N4', True, True, True, True, True, True)
 
 # Add a nodal lateral load of 50 kips at the left side of the frame
-MomentFrame.AddNodeLoad("N2", "FX", 50)
+# The lateral load will be added to a load case called 'Lateral'
+MomentFrame.AddNodeLoad('N2', 'FX', 50, 'Lateral')
+
+# Create a load combination with a factor of 1.0 applied to the 'Lateral' load case
+MomentFrame.AddLoadCombo('Combo 1', {'Lateral':1.0})
 
 # Analyze the frame
-MomentFrame.Analyze()
+MomentFrame.Analyze(check_statics=True)
 
-# Display the model for viewing, with the text height 5 model units (inches) high
-Visualization.RenderModel(MomentFrame, 5)
+# Display the deformed shape of the structure magnified 25 times with the text height 5 model units (inches) high
+Visualization.DeformedShape(MomentFrame, scale_factor=25, textHeight=5, combo_name='Combo 1')
 
 # Print the shear, moment, and deflection diagrams for member 'M3'
-MomentFrame.GetMember("M3").PlotShear("Fy")
-MomentFrame.GetMember("M3").PlotMoment("Mz")
-MomentFrame.GetMember("M3").PlotDeflection("dy")
+MomentFrame.GetMember('M3').PlotShear('Fy', 'Combo 1')
+MomentFrame.GetMember('M3').PlotMoment('Mz', 'Combo 1')
+MomentFrame.GetMember('M3').PlotDeflection('dy', 'Combo 1')
 
 # Print reactions at the bottom of the frame
-print("Left Support X Reaction: {Rxn:.2f} kip".format(Rxn = MomentFrame.GetNode("N1").RxnFX))
-print("Left Support Y Reaction: {Rxn:.2f} kip".format(Rxn = MomentFrame.GetNode("N1").RxnFY))
-print("Left Support Moment Reaction: {Rxn:.2f} kip-ft".format(Rxn = MomentFrame.GetNode("N1").RxnMZ))
+# PyNite stores reactions in each node as a dictionary of reactions with the keys being the load combination names
+print('Left Support X Reaction:', MomentFrame.GetNode('N1').RxnFX['Combo 1'], 'kip')
+print('Left Support Y Reaction:', MomentFrame.GetNode('N1').RxnFY['Combo 1'], 'kip')
+print('Left Support Moment Reaction:', MomentFrame.GetNode('N1').RxnMZ['Combo 1'], 'kip-in')
 
-print("Right Support X Reacton: {Rxn:.2f} kip".format(Rxn = MomentFrame.GetNode("N4").RxnFX))
-print("Right Support Y Reacton: {Rxn:.2f} kip".format(Rxn = MomentFrame.GetNode("N4").RxnFY))
-print("Right Support Moment Reacton: {Rxn:.2f} kip-ft".format(Rxn = MomentFrame.GetNode("N4").RxnMZ))
+print('Right Support X Reacton:', MomentFrame.GetNode('N4').RxnFX['Combo 1'], 'kip')
+print('Right Support Y Reacton:', MomentFrame.GetNode('N4').RxnFY['Combo 1'], 'kip')
+print('Right Support Moment Reacton:', MomentFrame.GetNode('N4').RxnMZ['Combo 1'], 'kip-in')
