280979f57429f141d2e48c1aaef4188c82090e27
Craig Brinck <cbrinck@outlook.com>
2020-09-28 10:31:39 -0600

    More work on isoparametric quad elements

--- PyNite/Quad3D.py
+++ PyNite/Quad3D.py
@@ -12,11 +12,13 @@
 from numpy import array, arccos, dot, cross, matmul, add, zeros, insert, transpose
 from numpy.linalg import inv, det, norm
 from math import atan, sin, cos
+from PyNite.LoadCombo import LoadCombo
 
 class Quad3D():
 
 #%%
-    def __init__(self, Name, iNode, jNode, mNode, nNode, t, E, nu):
+    def __init__(self, Name, iNode, jNode, mNode, nNode, t, E, nu,
+                 LoadCombos={'Combo 1':LoadCombo('Combo 1', factors={'Case 1':1.0})}):
 
         self.Name = Name
         self.ID = None
@@ -30,20 +32,58 @@ class Quad3D():
         self.E = E
         self.nu = nu
 
+        self.pressures = []  # A list of surface pressures [pressure, case='Case 1']
+        self.LoadCombos = LoadCombos
+
+#%%
+    def Hw(self, r, s):
+
+        Hw = 1/4*array([[(1+r)*(1+s), 0, 0, (1-r)*(1+s), 0, 0, (1-r)*(1-s), 0, 0, (1+r)*(1-s), 0, 0]])
+
+        return Hw
+
 #%%
     def J(self, r, s):
         '''
         Returns the Jacobian matrix for the element
         '''
-        x1, y1 = self.mNode.X, self.mNode.Y
-        x2, y2 = self.nNode.X, self.nNode.Y
-        x3, y3 = self.iNode.X, self.iNode.Y
-        x4, y4 = self.jNode.X, self.jNode.Y
+        
+        # The derivation of this matrix is adapted from "Finite Element Procedures"
+        X1, Y1, Z1 = self.mNode.X, self.mNode.Y, self.mNode.Z
+        X2, Y2, Z2 = self.nNode.X, self.nNode.Y, self.nNode.Z
+        X3, Y3, Z3 = self.iNode.X, self.iNode.Y, self.iNode.Z
+        X4, Y4, Z4 = self.jNode.X, self.jNode.Y, self.jNode.Z
+
+        # Following Reference 1, Figure 5.26, node 3 will be used as the
+        # origin of the plate's local (x, y) coordinate system. Find the
+        # vector from the origin to each node.
+        vector_32 = array([X2 - X3, Y2 - Y3, Z2 - Z3]).T
+        vector_31 = array([X1 - X3, Y1 - Y3, Z1 - Z3]).T
+        vector_34 = array([X4 - X3, Y4 - Y3, Z4 - Z3]).T
+
+        # Define the plate's local x, y, and z axes
+        x_axis = vector_34
+        z_axis = cross(x_axis, vector_32)
+        y_axis = cross(z_axis, x_axis)
+
+        # Convert the axes into unit vectors
+        x_axis = x_axis/norm(x_axis)
+        y_axis = y_axis/norm(y_axis)
+        z_axis = z_axis/norm(z_axis)
+
+        # Calculate the local (x, y) coordinates for each node
+        x1 = dot(vector_31, x_axis)
+        x2 = dot(vector_32, x_axis)
+        x3 = 0
+        x4 = dot(vector_34, x_axis)
+        y1 = dot(vector_31, y_axis)
+        y2 = dot(vector_32, y_axis)
+        y3 = 0
+        y4 = dot(vector_34, y_axis)
 
         # Return the Jacobian matrix
         return 1/4*array([[x1*(s + 1) - x2*(s + 1) + x3*(s - 1) - x4*(s - 1), y1*(s + 1) - y2*(s + 1) + y3*(s - 1) - y4*(s - 1)],
                           [x1*(r + 1) - x2*(r - 1) + x3*(r - 1) - x4*(r + 1), y1*(r + 1) - y2*(r - 1) + y3*(r - 1) - y4*(r + 1)]])
-
 #%%
     def B_kappa(self, r, s):
 
@@ -52,19 +92,41 @@ class Quad3D():
         # Row 2 = interpolation functions differentiated with respect to y
         # Note that the inverse of the Jacobian converts from derivatives with
         # respect to r and s to derivatives with respect to x and y
-        dH = matmul(inv(self.J(r, s)), 1/4*array([[s + 1, -s - 1, s - 1, -s + 1],                 
-                                                  [r + 1, -r + 1, r - 1, -r - 1]]))
+        dH = matmul(inv(self.J(r, s)), 1/4*array([[1 + s, -1 - s, -1 + s,  1 - s],                 
+                                                  [1 + r,  1 - r, -1 + r, -1 - r]]))
         
         # Row 1 = d(beta_x)/dx divided by the local displacement vector 'u'
         # Row 2 = d(beta_y)/dy divided by the local displacement vector 'u'
         # Row 3 = d(beta_x)/dy + d(beta_y)/dx divided by the local displacement vector 'u'
         # Note that beta_x is a function of -theta_y and beta_y is a function of +theta_x (Equations 5.99, p. 423)
         B_kappa = array([[0,    0,     -dH[0, 0], 0,    0,     -dH[0, 1], 0,    0,     -dH[0, 2], 0,    0,     -dH[0, 3]],
-                         [0, dH[1, 0],     0,     0, dH[1, 1],     0,     0, dH[1, 2],     0,     0, dH[1, 3],     0   ],
+                         [0, dH[1, 0],     0,     0, dH[1, 1],     0,     0, dH[1, 2],     0,     0, dH[1, 3],     0    ],
                          [0, dH[0, 0], -dH[1, 0], 0, dH[0, 1], -dH[1, 1], 0, dH[0, 2], -dH[1, 2], 0, dH[0, 3], -dH[1, 3]]])
 
         return B_kappa
-    
+
+#%%
+    def B_gamma(self, r, s):
+
+        H = 1/4*array([(1 + r)*(1 + s), (1 - r)*(1 + s), (1 - r)*(1 - s), (1 + r)*(1 - s)])
+
+        # Differentiate the interpolation functions
+        # Row 1 = interpolation functions differentiated with respect to x
+        # Row 2 = interpolation functions differentiated with respect to y
+        # Note that the inverse of the Jacobian converts from derivatives with
+        # respect to r and s to derivatives with respect to x and y
+        dH = matmul(inv(self.J(r, s)), 1/4*array([[1 + s, -1 - s, -1 + s,  1 - s],                 
+                                                  [1 + r,  1 - r, -1 + r, -1 - r]]))
+        
+        # Row 1 = d(beta_x)/dx divided by the local displacement vector 'u'
+        # Row 2 = d(beta_y)/dy divided by the local displacement vector 'u'
+        # Row 3 = d(beta_x)/dy + d(beta_y)/dx divided by the local displacement vector 'u'
+        # Note that beta_x is a function of -theta_y and beta_y is a function of +theta_x (Equations 5.99, p. 423)
+        B_gamma = array([[dH[0, 0],   0,   H[0], dH[0, 1],   0,   H[1], dH[0, 2],   0,   H[2], dH[0, 3],   0,   H[3]],
+                         [dH[1, 0], -H[0],  0,   dH[1, 1], -H[1],  0,   dH[1, 2], -H[2],  0,   dH[1, 3], -H[3],  0  ]])
+        
+        return B_gamma
+
 #%%
     def B_gamma_MITC4(self, r, s):
 
@@ -111,8 +173,8 @@ class Quad3D():
 
         # Find the angles between the axes of the natural coordinate system and
         # the local x-axis.
-        r_axis = array([(x2 + x3)/2 - (x1 + x4)/2, (y2 + y3)/2 - (y1 + y4)/2, 0]).T
-        s_axis = array([(x3 + x4)/2 - (x1 + x2)/2, (y3 + y4)/2 - (y1 + y2)/2, 0]).T
+        r_axis = array([(x1 + x4)/2 - (x2 + x3)/2, (y1 + y4)/2 - (y2 + y3)/2, 0]).T
+        s_axis = array([(x1 + x2)/2 - (x3 + x4)/2, (y1 + y2)/2 - (y3 + y4)/2, 0]).T
 
         r_axis = r_axis/norm(r_axis)
         s_axis = s_axis/norm(s_axis)
@@ -127,6 +189,7 @@ class Quad3D():
         gs = ((Ax + s*Bx)**2 + (Ay + s*By)**2)**0.5/(8*det(J))
 
         # See Jupyter Notebook derivation for this next part
+        gamma_rz = gr/4*array([[2*(s + 1), -s*y1 + s*y2 - y1 + y2, s*x1 - s*x2 + x1 - x2, 2*(-s - 1), -s*y1 + s*y2 - y1 + y2,  s*x1 - s*x2 + x1 - x2, 2*(s - 1), -s*y3 + s*y4 + y3 - y4,  s*x3 - s*x4 - x3 + x4, 2*(1 - s),  -s*y3 + s*y4 + y3 - y4, s*x3 - s*x4 - x3 + x4]])
         gamma_rz = gr/4*array([[2*(s + 1), -s*y1 + s*y2 - y1 + y2, s*x1 - s*x2 + x1 - x2, 2*(-s - 1), -s*y1 + s*y2 - y1 + y2,  s*x1 - s*x2 + x1 - x2, 2*(s - 1), -s*y3 + s*y4 + y3 - y4,  s*x3 - s*x4 - x3 + x4, 2*(1 - s),  -s*y3 + s*y4 + y3 - y4, s*x3 - s*x4 - x3 + x4]])
         gamma_sz = gs/4*array([[2*(r + 1), -r*y1 + r*y4 - y1 + y4, r*x1 - r*x4 + x1 - x4, 2*(1 - r),   r*y2 - r*y3 - y2 + y3, -r*x2 + r*x3 + x2 - x3, 2*(r - 1),  r*y2 - r*y3 - y2 + y3, -r*x2 + r*x3 + x2 - x3, 2*(-r - 1), -r*y1 + r*y4 - y1 + y4, r*x1 - r*x4 + x1 - x4]])
 
@@ -214,17 +277,25 @@ class Quad3D():
         Cb = self.Cb()
         Cs = self.Cs()
 
+        # Define the gauss point for numerical integration
+        gp = 1/3**0.5
+
         # See Reference 1, Equation 5.94
-        k = (matmul(self.B_kappa(-0.5773, -0.5773).T, matmul(Cb, self.B_kappa(-0.5773, -0.5773)))*det(self.J(-0.5773, -0.5773)) +
-             matmul(self.B_kappa(-0.5773, 0.5773).T, matmul(Cb, self.B_kappa(-0.5773, 0.5773)))*det(self.J(-0.5773, 0.5773)) +
-             matmul(self.B_kappa(0.5773, 0.5773).T, matmul(Cb, self.B_kappa(0.5773, 0.5773)))*det(self.J(0.5773, 0.5773)) +
-             matmul(self.B_kappa(0.5773, -0.5773).T, matmul(Cb, self.B_kappa(0.5773, -0.5773)))*det(self.J(0.5773, -0.5773)))
+        k = (matmul(self.B_kappa(-gp, -gp).T, matmul(Cb, self.B_kappa(-gp, -gp)))*det(self.J(-gp, -gp)) +
+             matmul(self.B_kappa(-gp, gp).T, matmul(Cb, self.B_kappa(-gp, gp)))*det(self.J(-gp, gp)) +
+             matmul(self.B_kappa(gp, gp).T, matmul(Cb, self.B_kappa(gp, gp)))*det(self.J(gp, gp)) +
+             matmul(self.B_kappa(gp, -gp).T, matmul(Cb, self.B_kappa(gp, -gp)))*det(self.J(gp, -gp)))
         
-        k += (matmul(self.B_gamma_MITC4(-0.5773, -0.5773).T, matmul(Cs, self.B_gamma_MITC4(-0.5773, -0.5773)))*det(self.J(-0.5773, -0.5773)) +
-              matmul(self.B_gamma_MITC4(-0.5773, 0.5773).T, matmul(Cs, self.B_gamma_MITC4(-0.5773, 0.5773)))*det(self.J(-0.5773, 0.5773)) +
-              matmul(self.B_gamma_MITC4(0.5773, 0.5773).T, matmul(Cs, self.B_gamma_MITC4(0.5773, 0.5773)))*det(self.J(0.5773, 0.5773)) +
-              matmul(self.B_gamma_MITC4(0.5773, -0.5773).T, matmul(Cs, self.B_gamma_MITC4(0.5773, -0.5773)))*det(self.J(0.5773, -0.5773)))
+        k += (matmul(self.B_gamma_MITC4(-gp, -gp).T, matmul(Cs, self.B_gamma_MITC4(-gp, -gp)))*det(self.J(-gp, -gp)) +
+              matmul(self.B_gamma_MITC4(-gp, gp).T, matmul(Cs, self.B_gamma_MITC4(-gp, gp)))*det(self.J(-gp, gp)) +
+              matmul(self.B_gamma_MITC4(gp, gp).T, matmul(Cs, self.B_gamma_MITC4(gp, gp)))*det(self.J(gp, gp)) +
+              matmul(self.B_gamma_MITC4(gp, -gp).T, matmul(Cs, self.B_gamma_MITC4(gp, -gp)))*det(self.J(gp, -gp)))
         
+        # Calculate the stiffness of a weak spring for the drilling degree of freedom (rotation about local z)
+        k_rz = min(abs(k[1, 1]), abs(k[2, 2]), abs(k[4, 4]), abs(k[5, 5]),
+                   abs(k[7, 7]), abs(k[8, 8]), abs(k[10, 10]), abs(k[11, 11])
+                   )/1000
+
         # Insert rows of zeros for degrees of freedom not included in the matrix above
         k = insert(k, 12, 0, axis=0)
         k = insert(k, 12, 0, axis=1)
@@ -245,6 +316,7 @@ class Quad3D():
         k = insert(k, 6, 0, axis=0)
         k = insert(k, 6, 0, axis=1)
 
+
         k = insert(k, 3, 0, axis=0)
         k = insert(k, 3, 0, axis=1)
         k = insert(k, 3, 0, axis=0)
@@ -257,6 +329,12 @@ class Quad3D():
         k = insert(k, 0, 0, axis=1)
         k = insert(k, 0, 0, axis=0)
         k = insert(k, 0, 0, axis=1)
+
+        # Add the drilling degree of freedom's weak spring
+        k[5, 5] = k_rz
+        k[11, 11] = k_rz
+        k[17, 17] = k_rz
+        k[23, 23] = k_rz
 
         return k
 
@@ -271,11 +349,14 @@ class Quad3D():
         t = self.t
         C = self.C()
 
+        # Define the gauss point for numerical integration
+        gp = 1/3**0.5
+
         # See reference 1 at the bottom of page 353, and reference 2 page 466
-        k = t*(matmul(self.B_m(-0.5773, -0.5773).T, matmul(C, self.B_m(-0.5773, -0.5773)))*det(self.J(-0.5773, -0.5773)) +
-               matmul(self.B_m(-0.5773, 0.5773).T, matmul(C, self.B_m(-0.5773, 0.5773)))*det(self.J(-0.5773, 0.5773)) +
-               matmul(self.B_m(0.5773, 0.5773).T, matmul(C, self.B_m(0.5773, 0.5773)))*det(self.J(0.5773, 0.5773)) +
-               matmul(self.B_m(0.5773, -0.5773).T, matmul(C, self.B_m(0.5773, -0.5773)))*det(self.J(0.5773, -0.5773)))
+        k = t*(matmul(self.B_m(-gp, -gp).T, matmul(C, self.B_m(-gp, -gp)))*det(self.J(-gp, -gp)) +
+               matmul(self.B_m(-gp, gp).T, matmul(C, self.B_m(-gp, gp)))*det(self.J(-gp, gp)) +
+               matmul(self.B_m(gp, gp).T, matmul(C, self.B_m(gp, gp)))*det(self.J(gp, gp)) +
+               matmul(self.B_m(gp, -gp).T, matmul(C, self.B_m(gp, -gp)))*det(self.J(gp, -gp)))
         
         # Insert rows of zeros for degrees of freedom not included in the matrix above
         k = insert(k, 8, 0, axis=0)
@@ -313,6 +394,8 @@ class Quad3D():
         k = insert(k, 2, 0, axis=1)
         k = insert(k, 2, 0, axis=0)
         k = insert(k, 2, 0, axis=1)
+
+
         
         return k
 
@@ -336,11 +419,66 @@ class Quad3D():
 #%%
     def fer(self, combo_name='Combo 1'):
         '''
-        Returns the quad element's local fixed end reaction vector (zero's for
+        Returns the quadrilateral's local fixed end reaction vector (zero's for
         now until surface loads get added).
-        '''
 
-        return zeros((24, 1))
+        Parameters
+        ----------
+        combo_name : string
+            The name of the load combination to get the consistent load vector
+            for.
+        '''
+        
+        # Initialize the fixed end reaction vector
+        fer = zeros((12,1))
+
+        # Get the requested load combination
+        combo = self.LoadCombos[combo_name]
+
+        # Define the gauss point used for numerical integration
+        gp = 1/3**0.5
+
+        # Loop through each load case and factor in the load combination
+        for case, factor in combo.factors.items():
+            
+            p = 0
+
+            # Sum the pressures
+            for pressure in self.pressures:
+
+                # Check if the current point load corresponds to the current load case
+                if pressure[1] == case:
+
+                    # Sum the pressures
+                    p -= factor*pressure[0]
+        
+        fer = (self.Hw(-gp, -gp).T*p*det(self.J(-gp, -gp))
+               + self.Hw(-gp, gp).T*p*det(self.J(-gp, gp))
+               + self.Hw(gp, gp).T*p*det(self.J(gp, gp))
+               + self.Hw(gp, -gp).T*p*det(self.J(gp, -gp)))
+
+        # Insert rows of zeros for degrees of freedom not included in the matrix above
+        fer = insert(fer, 12, 0, axis=0)
+
+        fer = insert(fer, 9, 0, axis=0)
+        fer = insert(fer, 9, 0, axis=0)
+
+        fer = insert(fer, 9, 0, axis=0)
+
+        fer = insert(fer, 6, 0, axis=0)
+        fer = insert(fer, 6, 0, axis=0)
+
+        fer = insert(fer, 6, 0, axis=0)
+
+        fer = insert(fer, 3, 0, axis=0)
+        fer = insert(fer, 3, 0, axis=0)
+
+        fer = insert(fer, 3, 0, axis=0)
+
+        fer = insert(fer, 0, 0, axis=0)
+        fer = insert(fer, 0, 0, axis=0)
+
+        return fer
 
 #%%
     def d(self, combo_name='Combo 1'):
@@ -416,6 +554,22 @@ class Quad3D():
         
         # Calculate and return the stiffness matrix in global coordinates
         return matmul(matmul(transpose(self.T()), self.k()), self.T())
+
+#%% 
+    # Global fixed end reaction vector
+    def FER(self, combo_name='Combo 1'):
+        '''
+        Returns the global fixed end reaction vector.
+
+        Parameters
+        ----------
+        combo_name : string
+            The name of the load combination to calculate the fixed end
+            reaction vector for (not the load combination itself).
+        '''
+        
+        # Calculate and return the fixed end reaction vector
+        return matmul(inv(self.T()), self.fer(combo_name))
 
 #%%  
     def T(self):
@@ -530,8 +684,33 @@ class Quad3D():
         # Slice out terms not related to plate bending
         d = self.d(combo_name)[[2, 3, 4, 8, 9, 10, 14, 15, 16, 20, 21, 22], :]
 
-        # Calculate and return internal moments
-        return matmul(self.Cb(), matmul(self.B_kappa(r, s), d))
+        # Define extrapolated r and s points
+        r_ex = r*3**0.5
+        s_ex = s*3**0.5 
+
+        # Define the interpolation functions
+        H = 1/4*array([(1 + r_ex)*(1 + s_ex), (1 - r_ex)*(1 + s_ex), (1 - r_ex)*(1 - s_ex), (1 + r_ex)*(1 - s_ex)])
+
+        # Define the gauss point
+        gp = 1/3**0.5
+
+        # Get the stress-strain matrix
+        Cb = self.Cb()
+
+        # Calculate the internal moments [Mx, My, Mxy] at each gauss point
+        m1 = matmul(Cb, matmul(self.B_kappa(gp, gp), d))
+        m2 = matmul(Cb, matmul(self.B_kappa(-gp, gp), d))
+        m3 = matmul(Cb, matmul(self.B_kappa(-gp, -gp), d))
+        m4 = matmul(Cb, matmul(self.B_kappa(gp, -gp), d))
+
+        # Extrapolate to get the value at the requested location
+        Mx = H[0]*m1[0] + H[1]*m2[0] + H[2]*m3[0] + H[3]*m4[0]
+        My = H[0]*m1[1] + H[1]*m2[1] + H[2]*m3[1] + H[3]*m4[1]
+        Mxy = H[0]*m1[2] + H[1]*m2[2] + H[2]*m3[2] + H[3]*m4[2]
+
+        return array([[Mx],
+                      [My],
+                      [Mxy]])
 
 #%%
     def membrane(self, r=0, s=0, combo_name='Combo 1'):
