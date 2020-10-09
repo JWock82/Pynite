c94bd24abb2a017bbf06dc325d459c4e114ecd55
Craig Brinck <cbrinck@outlook.com>
2020-10-08 21:51:05 -0600

    Added setup.py to repository

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyNiteFEA",
+    version="0.0.21",
+    author="D. Craig Brinck, PE, SE",
+    author_email="Building.Code@outlook.com",
+    description="An elastic 3D structural finite element library for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JWock82/PyNite.git",
+    packages=setuptools.find_packages(),
+    package_data = {'PyNite': ['*html', '*.css']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'PrettyTable'
+    ],
+    extras_require = {
+        'Visualization':  ['vtk'],
+        'Reporting': ['pdfkit', 'Jinja2'],
+        'Reviewing Derivations': ['jupyterlab', 'sympy']
+    },
+    include_package_data = True,
+    python_requires = '>=3.6',
+)
