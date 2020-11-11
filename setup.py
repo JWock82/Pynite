339292c8dc951ff8a494f7f2afd72e2f99a5486f
Craig Brinck <cbrinck@outlook.com>
2020-11-10 20:23:42 -0700

    Updated for v. 0.0.24

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyNiteFEA",
-    version="0.0.23",
+    version="0.0.24",
     author="D. Craig Brinck, PE, SE",
     author_email="Building.Code@outlook.com",
     description="An elastic 3D structural finite element library for Python.",
