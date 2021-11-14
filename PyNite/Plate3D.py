d174ee648fc7675f9c93d39cdd1b1e405418d48d
Craig <33323539+JWock82@users.noreply.github.com>
2021-11-14 07:47:19 -0500

    Update Plate3D.py
    Adjusted drilling stiffness to match Bathe’s recommendations.

--- PyNite/Plate3D.py
+++ PyNite/Plate3D.py
@@ -106,7 +106,7 @@ class Plate3D():
         # rotational stiffnesses in the matrix.
         k_rz = min(abs(k[1, 1]), abs(k[2, 2]), abs(k[4, 4]), abs(k[5, 5]),
                    abs(k[7, 7]), abs(k[8, 8]), abs(k[10, 10]), abs(k[11, 11])
-                   )/1000000
+                   )/1000
 
         # The matrix currently only holds terms related to bending action. We need to expand it to
         # with placeholders for all the degrees of freedom so it can be directly added to the
