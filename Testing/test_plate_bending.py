815dd27bd3c7d21432d7c1e4d843a8de2aa8bed0
Craig Brinck <cbrinck@outlook.com>
2023-03-11 10:21:22 -0500

    Updated tests for new materials feature

--- Testing/test_plate_bending.py
+++ Testing/test_plate_bending.py
@@ -34,48 +34,50 @@ class Test_Plates(unittest.TestCase):
         # Units for this model are pounds and inches
         """
 
-        plModel= FEModel3D()
+        plate_model= FEModel3D()
 
-        plModel.add_node('N1', 0, 0, 0)
-        plModel.add_node('N2', 10, 0, 0)
-        plModel.add_node('N3', 20, 0, 0)
-        plModel.add_node('N4', 0, 10, 0)
-        plModel.add_node('N5', 10, 10, 0)
-        plModel.add_node('N6', 20, 10, 0)
-        plModel.add_node('N7', 0, 20, 0)
-        plModel.add_node('N8', 10, 20, 0)
-        plModel.add_node('N9', 20, 20, 0)
+        plate_model.add_node('N1', 0, 0, 0)
+        plate_model.add_node('N2', 10, 0, 0)
+        plate_model.add_node('N3', 20, 0, 0)
+        plate_model.add_node('N4', 0, 10, 0)
+        plate_model.add_node('N5', 10, 10, 0)
+        plate_model.add_node('N6', 20, 10, 0)
+        plate_model.add_node('N7', 0, 20, 0)
+        plate_model.add_node('N8', 10, 20, 0)
+        plate_model.add_node('N9', 20, 20, 0)
 
-        plModel.add_plate('P1', 'N1', 'N2', 'N5', 'N4', 0.1, 30000000, 0.3)
-        plModel.add_plate('P2', 'N2', 'N3', 'N6', 'N5', 0.1, 30000000, 0.3)
-        plModel.add_plate('P3', 'N4', 'N5', 'N8', 'N7', 0.1, 30000000, 0.3)
-        plModel.add_plate('P4', 'N5', 'N6', 'N9', 'N8', 0.1, 30000000, 0.3)
+        plate_model.add_material('Steel', 30000000, 11200000, 0.3, 0.284)
 
-        plModel.add_node_load('N5', 'FZ', -100)
+        plate_model.add_plate('P1', 'N1', 'N2', 'N5', 'N4', 0.1, 'Steel')
+        plate_model.add_plate('P2', 'N2', 'N3', 'N6', 'N5', 0.1, 'Steel')
+        plate_model.add_plate('P3', 'N4', 'N5', 'N8', 'N7', 0.1, 'Steel')
+        plate_model.add_plate('P4', 'N5', 'N6', 'N9', 'N8', 0.1, 'Steel')
 
-        plModel.def_support('N1', True, True, True, True, True, True)
-        plModel.def_support('N2', True, True, True, True, True, True)
-        plModel.def_support('N3', True, True, True, True, True, True)
-        plModel.def_support('N4', True, True, True, True, True, True)
-        plModel.def_support('N6', True, True, True, True, True, True)
-        plModel.def_support('N7', True, True, True, True, True, True)
-        plModel.def_support('N8', True, True, True, True, True, True)
-        plModel.def_support('N9', True, True, True, True, True, True)
+        plate_model.add_node_load('N5', 'FZ', -100)
 
-        plModel.def_support('N5', True, True, False, False, False, True)
+        plate_model.def_support('N1', True, True, True, True, True, True)
+        plate_model.def_support('N2', True, True, True, True, True, True)
+        plate_model.def_support('N3', True, True, True, True, True, True)
+        plate_model.def_support('N4', True, True, True, True, True, True)
+        plate_model.def_support('N6', True, True, True, True, True, True)
+        plate_model.def_support('N7', True, True, True, True, True, True)
+        plate_model.def_support('N8', True, True, True, True, True, True)
+        plate_model.def_support('N9', True, True, True, True, True, True)
+
+        plate_model.def_support('N5', True, True, False, False, False, True)
 
         # Check to see if the stiffness matrix for each plate is symmetric
-        # print(allclose(plModel.Plates[0].K(), plModel.Plates[0].K().T))
-        # print(allclose(plModel.Plates[1].K(), plModel.Plates[1].K().T))
-        # print(allclose(plModel.Plates[2].K(), plModel.Plates[2].K().T))
-        # print(allclose(plModel.Plates[3].K(), plModel.Plates[3].K().T))
+        # print(allclose(plate_model.Plates[0].K(), plate_model.Plates[0].K().T))
+        # print(allclose(plate_model.Plates[1].K(), plate_model.Plates[1].K().T))
+        # print(allclose(plate_model.Plates[2].K(), plate_model.Plates[2].K().T))
+        # print(allclose(plate_model.Plates[3].K(), plate_model.Plates[3].K().T))
 
         # Check to see if the global stiffness matrix is symmetric
-        # print(allclose(plModel.K(Renumber=True), plModel.K(Renumber=False).T))
+        # print(allclose(plate_model.K(Renumber=True), plate_model.K(Renumber=False).T))
 
-        plModel.analyze(check_statics=True, sparse=False)
+        plate_model.analyze(check_statics=True, sparse=False)
         # Test: displacement of N5 in Z direction
-        calculated_displacement = plModel.Nodes['N5'].DZ['Combo 1']
+        calculated_displacement = plate_model.Nodes['N5'].DZ['Combo 1']
         expected_displacement = -0.0861742424242424
         self.assertAlmostEqual(calculated_displacement/expected_displacement, 1.0, 2)
 
