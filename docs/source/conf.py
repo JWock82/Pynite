08d8a193b608e006b2c58ec250dc26cc7895d8e8
Craig Brinck <cbrinck@outlook.com>
2023-03-24 11:49:20 -0600

    Updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,11 @@
 
 import os
 import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('...'))
+sys.path.insert(0, os.path.abspath('....'))
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
