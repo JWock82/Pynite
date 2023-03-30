8c45b61c1e44e7215c7b8cf87e4dedb46ed879d3
Craig Brinck <cbrinck@outlook.com>
2023-03-29 21:30:18 -0400

    Changed theme. Increased sidebar width.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'haiku'
+html_theme = 'alabaster'
+html_theme_options = {
+    "sidebarwidth": "300px"
+}
 html_static_path = ['_static']
