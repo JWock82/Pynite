4254bbad2e0d7e71e80cee4367f4f6a305851369
Craig Brinck <cbrinck@outlook.com>
2023-11-09 19:35:58 -0500

    Changed the order of import statements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,9 @@ sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../PyNite'))
 sys.path.insert(0, os.path.abspath('../../PyNite/FEModel3D'))
 
-from PyNite import FEModel3D
-
 # autodoc settings
 autodoc_mock_imports = ['numpy']  # Mock the numpy import
+from PyNite import FEModel3D
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
