8ad80cb0e53edba6fbd4c1328a43c0c530115244
Craig Brinck <cbrinck@outlook.com>
2023-11-09 19:24:49 -0500

    Added FEModel3D to sys.path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../PyNite'))
+sys.path.insert(0, os.path.abspath('../../PyNite/FEModel3D'))
 
 # autodoc settings
 autodoc_mock_imports = ['numpy']  # Mock the numpy import
