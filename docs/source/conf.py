ddd37ddf1937cfaf97e16b8f9bf3ec2ab913c23f
Craig Brinck <cbrinck@outlook.com>
2023-11-09 19:03:04 -0500

    Added numpy to imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../PyNite'))
+
 from PyNite import FEModel3D
+import numpy
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
