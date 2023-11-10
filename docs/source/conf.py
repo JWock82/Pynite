19d7d6876a8c8f40e24869d9f6df11dbc3791725
Craig Brinck <cbrinck@outlook.com>
2023-11-09 19:01:01 -0500

    imported FEModel3D

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../PyNite'))
-sys.path.insert(0, os.path.abspath('../../PyNite/FEModel3D'))
+from PyNite import FEModel3D
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
