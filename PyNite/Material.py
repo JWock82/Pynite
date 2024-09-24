ed7456153869610b37662fb556d9abf34e810bb1
bjhowie <45848617+bjhowie@users.noreply.github.com>
2024-09-24 22:25:12 +0800

    Fix TC test + remove 3.10 style type hints

--- PyNite/Material.py
+++ PyNite/Material.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 class Material():
     """
     A class representing a material assigned to a Member3D, Plate or Quad in a finite element model.
 
     This class stores all properties related to the physical material of the element
     """
-    def __init__(self, model, name:str, E:float, G:float, nu:float, rho:float, fy:float | None = None):
+    def __init__(self, model, name:str, E:float, G:float, nu:float, rho:float, fy:Optional[float] = None):
 
         self.model = model
         self.name = name
