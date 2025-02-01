1e1cbdc8f8dd4892a7c1bd09a7c99bf9f6755309
JWock82 <BuildingCode@outlook.com>
2025-02-01 10:21:18 -0700

    Corrected end support conditions

--- Testing/test_beam_rotation.py
+++ Testing/test_beam_rotation.py
@@ -9,7 +9,7 @@ def test_beam_rotation():
     beam.add_node('N1', 0, 0, 0)
     beam.add_node('N2', 10, 0, 0)
     beam.def_support('N1', True, True, True, True, False, False)
-    beam.def_support('N2', False, True, True, True, True, True)
+    beam.def_support('N2', False, True, True, False, False, False)
     beam.add_material('Steel', 29000/12**2, 11200/12**2, 0.3, 0.490, 60)
     beam.add_section('W12x26', 7.65/12**2, 17.3/12**4, 204/12**4, 0.3/12**4)
     beam.add_member('M1', 'N1', 'N2', 'Steel', 'W12x26', 45)
