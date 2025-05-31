edb23ca971103ea965c85fb2768b285917b0be8e
JWock82 <BuildingCode@outlook.com>
2025-05-31 08:15:49 -0600

    Commented out plot tests

--- Testing/test_continuous_beam.py
+++ Testing/test_continuous_beam.py
@@ -1,6 +1,6 @@
 from Pynite import FEModel3D
 import math
-import matplotlib, matplotlib.pyplot as plt
+# import matplotlib, matplotlib.pyplot as plt
 
 def create_model():
 
@@ -32,24 +32,24 @@ def test_continuous_beam_moments():
     assert math.isclose(member.max_moment('Mz', '1.0D'), 18000.0), 'Incorrect max moment during continuous beam test'
     assert math.isclose(max(member.moment_array('Mz', 11, '1.0D')[1]), 18000.00), 'Incorrect max moment in moment array during continuous beam test.'
 
-def test_plots():
+# def test_plots():
 
-    model = create_model()
+#     model = create_model()
 
-    assert isinstance(model.members['M1'].plot_shear('Fy', '1.0D', 100), matplotlib.figure.Figure)
-    plt.close()
+#     assert isinstance(model.members['M1'].plot_shear('Fy', '1.0D', 100), matplotlib.figure.Figure)
+#     plt.close()
 
-    assert isinstance(model.members['M1'].plot_moment('Mz', '1.0D', 100), matplotlib.figure.Figure)
-    plt.close()
+#     assert isinstance(model.members['M1'].plot_moment('Mz', '1.0D', 100), matplotlib.figure.Figure)
+#     plt.close()
     
-    assert isinstance(model.members['M1'].plot_torque('1.0D', 100), matplotlib.figure.Figure)
-    plt.close()
+#     assert isinstance(model.members['M1'].plot_torque('1.0D', 100), matplotlib.figure.Figure)
+#     plt.close()
     
-    assert isinstance(model.members['M1'].plot_axial('1.0D', 100), matplotlib.figure.Figure)
-    plt.close()
+#     assert isinstance(model.members['M1'].plot_axial('1.0D', 100), matplotlib.figure.Figure)
+#     plt.close()
     
-    assert isinstance(model.members['M1'].plot_deflection('dy', '1.0D', 100), matplotlib.figure.Figure)
-    plt.close()
+#     assert isinstance(model.members['M1'].plot_deflection('dy', '1.0D', 100), matplotlib.figure.Figure)
+#     plt.close()
 
 if __name__ == '__main__':
     test_continuous_beam_moments()
