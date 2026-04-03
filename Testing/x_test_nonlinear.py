a57e678a7113b43f551b9abbcfdede64d254d27c
Craig Brinck <cbrinck@outlook.com>
2026-04-03 08:12:55 -0600

    Progress toward pushover test

--- Testing/x_test_nonlinear.py
+++ Testing/x_test_nonlinear.py
@@ -23,20 +23,18 @@ def test_plastic_beam():
 
     # Add nodes
     plastic_beam.add_node('N1', 0, 0, 0)
-    plastic_beam.add_node('N2', 8*12, 0, 0)
-    plastic_beam.add_node('N3', 24*12, 0, 0)
+    plastic_beam.add_node('N2', 24*12, 0, 0)
 
     # Add supports
     plastic_beam.def_support('N1', True, True, True, True, True, True)
-    plastic_beam.def_support('N3', False, True, True, False, False, False)
+    plastic_beam.def_support('N2', False, True, True, False, False, False)
 
     # Add a member
     plastic_beam.add_member('M1', 'N1', 'N2', 'Stl_A992', 'W12x65')
-    plastic_beam.add_member('M2', 'N2', 'N3', 'Stl_A992', 'W12x65')
 
     # Add a load
-    plastic_beam.add_node_load('N2', 'FY', 0.3, 'Push')
-    plastic_beam.add_node_load('N3', 'FX', -1.0, 'Push')
+    plastic_beam.add_member_pt_load('M1', 'Fy', -1.0, 8*12, 'Push')
+    plastic_beam.add_node_load('N2', 'FX', -1.0, 'Push')
 
     # Add a load combination
     plastic_beam.add_load_combo('Pushover', {'Push': 1.0})
@@ -45,10 +43,13 @@ def test_plastic_beam():
     plastic_beam._not_ready_yet_analyze_pushover(log=True, check_stability=False, push_combo='Pushover', max_iter=30, tol=0.01, sparse=False)
 
     # Get the resulting moments
-    calculated_moment = plastic_beam.members['M1'].min_moment('Mz')
-    expected_moment = -0.5*(10*12)**2/8
+    M_a = plastic_beam.members['M1'].moment('Mz', x=0.0, combo_name='Pushover')
+    M_b = plastic_beam.members['M1'].moment('Mz', x=8.0*12.0, combo_name='Pushover')
+    M_ae = 3752
+    M_be = -3752
 
-    assert isclose(calculated_moment, expected_moment), f'Calculated Moment: {calculated_moment} \nExpected Moment: {expected_moment}'
+    assert isclose(M_a, M_ae), f'Calculated Moment: {M_a} \nExpected Moment: {M_ae}'
+    assert isclose(M_b, M_be), f'Calculated Moment: {M_b} \nExpected Moment: {M_be}'
 
 
 if __name__ == '__main__':
