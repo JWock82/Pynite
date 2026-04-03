ac47417e8559f2d2fe1b1e7e81221a69a1ec6c36
Craig Brinck <cbrinck@outlook.com>
2026-04-03 08:06:38 -0600

    Updates/corrections to pushover test

--- Testing/x_test_nonlinear.py
+++ Testing/x_test_nonlinear.py
@@ -1,64 +1,56 @@
-import unittest
+from math import isclose
 from Pynite import FEModel3D
-from Pynite.Section import SteelSection
-import math
-import sys
-from io import StringIO
 
-class Test_End_Release(unittest.TestCase):
-    """Nonlinear material tests
+
+def test_plastic_beam():
+    """
+    Matrix Structural Analysis, 2nd Edition - Examples 8.6 (p. 228) & 10.4 (p. 282)
     """
 
-    def setUp(self):
-        # Suppress printed output temporarily
-        sys.stdout = StringIO()
+    # Create the model
+    plastic_beam = FEModel3D()
 
-    def tearDown(self):
-        # Reset the print function to normal
-        sys.stdout = sys.__stdout__
+    # Define a material
+    E = 29000  # ksi
+    G = 11200  # ksi
+    nu = 0.3
+    rho = 0.490/12**3  # kci
+    fy = 50  # ksi
+    plastic_beam.add_material('Stl_A992', E, G, nu, rho, fy)
 
-    def test_plastic_beam(self):
-        """Matrix Structural Analysis, 2nd Edition - Example 8.6
-        """
-        
-        # Create the model
-        plastic_beam = FEModel3D()
+    # Define a cross-section
+    plastic_beam.add_steel_section('W12x65', 19.1, 20, 533, 1, 15, 96.8, 'Stl_A992')
 
-        # Define a material
-        E = 29000  # ksi
-        G = 11200  # ksi
-        nu = 0.3
-        rho = 0.490/12**3  # kci
-        fy = 50  # ksi
-        plastic_beam.add_material('Stl_A992', E, G, nu, rho, fy)
+    # Add nodes
+    plastic_beam.add_node('N1', 0, 0, 0)
+    plastic_beam.add_node('N2', 8*12, 0, 0)
+    plastic_beam.add_node('N3', 24*12, 0, 0)
 
-        # Define a cross-section
-        plastic_beam.add_steel_section('W12x65', 19.1, 20, 533, 1, 15, 96.8, 'Stl_A992')
+    # Add supports
+    plastic_beam.def_support('N1', True, True, True, True, True, True)
+    plastic_beam.def_support('N3', False, True, True, False, False, False)
 
-        # Add nodes
-        plastic_beam.add_node('N1', 0, 0, 0)
-        plastic_beam.add_node('N2', 8*12, 0, 0)
-        plastic_beam.add_node('N3', 24*12, 0, 0)
+    # Add a member
+    plastic_beam.add_member('M1', 'N1', 'N2', 'Stl_A992', 'W12x65')
+    plastic_beam.add_member('M2', 'N2', 'N3', 'Stl_A992', 'W12x65')
 
-        # Add supports
-        plastic_beam.def_support('N1', True, True, True, True, True, True)
-        plastic_beam.def_support('N2', False, True, True, False, False, False)
-        
-        # Add a member
-        plastic_beam.add_member('M1', 'N1', 'N2', 'A992', 'W12x65')
+    # Add a load
+    plastic_beam.add_node_load('N2', 'FY', 0.3, 'Push')
+    plastic_beam.add_node_load('N3', 'FX', -1.0, 'Push')
 
-        # Add a load
-        plastic_beam.add_node_load('N2', 'FY', 0.3, 'Push')
-        plastic_beam.add_node_load('N3', 'FX', -1, 'Push')
+    # Add a load combination
+    plastic_beam.add_load_combo('Pushover', {'Push': 1.0})
 
-        # Add a load combination
-        plastic_beam.add_load_combo('Pushover', {'Push':1})
+    # Analysis the model
+    plastic_beam._not_ready_yet_analyze_pushover(log=True, check_stability=False, push_combo='Pushover', max_iter=30, tol=0.01, sparse=False)
 
-        # Analysis the model
-        plastic_beam.analyze_pushover(True, True, True, 'Pushover', 1.02, 20, 30, True)
+    # Get the resulting moments
+    calculated_moment = plastic_beam.members['M1'].min_moment('Mz')
+    expected_moment = -0.5*(10*12)**2/8
 
-        # Get the resulting moments
-        # calculated_moment = myModel.members['M1'].min_moment('Mz')
-        # expected_moment = -0.5*(10*12)**2/8
-        
-        # self.assertAlmostEqual(calculated_moment, expected_moment)
+    assert isclose(calculated_moment, expected_moment), f'Calculated Moment: {calculated_moment} \nExpected Moment: {expected_moment}'
+
+
+if __name__ == '__main__':
+
+    test_plastic_beam()
