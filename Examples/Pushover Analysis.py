aff78016ba9f22b5a12ef812b60be288c7a381c9
Craig Brinck <cbrinck@outlook.com>
2026-04-20 09:08:02 -0600

    Updated example to match latest pushover API

--- Examples/Pushover Analysis.py
+++ Examples/Pushover Analysis.py
@@ -35,7 +35,7 @@ plastic_beam.add_member('M1', 'N1', 'N3', 'Stl_A992', 'W12x65')
 
 # Define the pushover loads.
 # P = 259.3  # This load causes the first plastic hinge to form at the fixed end (N1)
-P = 346.2  # This load causes a second plastic hinge to form at the load point (N2).
+P = 325.7  # This load causes a second plastic hinge to form at the load point (N2).
 plastic_beam.add_node_load('N2', 'FY', -0.3*P, 'Push')
 plastic_beam.add_node_load('N3', 'FX', -P, 'Push')
 
